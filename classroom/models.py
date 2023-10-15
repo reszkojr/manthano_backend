0ac2a1f2c3523f583a2fce7af381efb4a5f3bbcb
reszkojr <s.reszkojr@gmail.com>
2023-10-15 07:49:55 -0300

    Classroom and Channel models

---
+++ classroom/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Classroom(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=11, default='')
+    description = models.TextField()
+    schedule = models.ImageField(upload_to='uploads/schedule_%Y_%m_%d')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=255)
+    group_name = models.CharField(max_length=100)
+
+    classroom = models.ForeignKey('classroom.Classroom', on_delete=models.CASCADE, default='', related_name='channels')
+
+    def __str__(self):
+        return self.name
--- manthano_backend/settings.py
+++ manthano_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "authentication",
+    "classroom",
     "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
