0de36a352744953014c731757ba25a1a361dd116
reszkojr <s.reszkojr@gmail.com>
2023-10-16 03:18:08 -0300

    authentication classes fix on settings

--- manthano_backend/settings.py
+++ manthano_backend/settings.py
@@ -73,13 +73,10 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
-    ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
 
