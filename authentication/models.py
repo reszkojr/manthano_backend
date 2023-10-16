c3182297cc6bd99471ba8dc1b338ac222a01c7f6
reszkojr <s.reszkojr@gmail.com>
2023-10-16 03:17:32 -0300

    "classroom" attribute on User model

--- authentication/models.py
+++ authentication/models.py
@@ -2,27 +2,29 @@ from datetime import timezone
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-    
+
 class ManthanoUserManager(BaseUserManager):
     def _create_user(self, username, email, password, is_staff, is_superuser, is_active, **extra_fields):
         now = timezone.now()
         if not username:
             raise ValueError('The given username must be set')
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, is_staff=is_staff, is_superuser=is_superuser, is_active=is_active, last_login=now, date_joined=now, **extra_fields)
+        user = self.model(username=username, email=email, is_staff=is_staff, is_superuser=is_superuser,
+                          is_active=is_active, last_login=now, date_joined=now, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_user(self, username, email=None, password=None, **extra_fields):
         return self._create_user(username, email, password, False, False, **extra_fields)
-    
+
     def create_superuser(self, username, email, password, **extra_fields):
         user = self._create_user(username, email, password, True, True, **extra_fields)
-    
-        user.is_active=True
+
+        user.is_active = True
         user.save(using=self._db)
         return user
+
 
 class ManthanoUser(AbstractUser):
     username = models.CharField('Username', max_length=32)
@@ -33,7 +35,8 @@ class ManthanoUser(AbstractUser):
     is_active = models.BooleanField('Active', default=True)
     date_joined = models.DateTimeField('Date joined', auto_now_add=True)
 
-    # classroom = models.ForeignKey('classrooms.Classroom', on_delete=models.CASCADE, related_name='classrooms', null=True)
+    classroom = models.ForeignKey('classroom.Classroom', on_delete=models.CASCADE,
+                                  related_name='classrooms', null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'password']
@@ -46,10 +49,11 @@ class ManthanoUser(AbstractUser):
 
     def get_full_name(self):
         return ('%s %s' % (self.first_name, self.last_name)).strip()
-    
+
     def get_short_name(self):
         return self.first_name
-    
+
+
 class Profile(models.Model):
     user = models.OneToOneField("authentication.ManthanoUser", on_delete=models.CASCADE)
 
