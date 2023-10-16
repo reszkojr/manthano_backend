d7946ed6aae89f2029a31166f4f9d1205cd79e57
reszkojr <s.reszkojr@gmail.com>
2023-10-16 03:18:18 -0300

    CreateClassroom APIView

--- classroom/urls.py
+++ classroom/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:classroom_code>/', views.classroom_home, name="classroom_home")
+    # path('<int:classroom_code>/', views.classroom_home, name="classroom_home"),
+    path('create/', views.CreateClassroom.as_view(), name='create_classroom'),
+    # path('<slug:code>/', views.classroom, name='classroom'),
+    # path('<slug:code>/<slug:channel>', views.classroom, name='classroom'),
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework_simplejwt import authentication
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-def classroom_home(request, classroom_code):
-    print(f'Hello world, {request.user}!')
+from classroom.serializers import ClassroomSerializer
+
+
+class CreateClassroom(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        serializer = ClassroomSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
