dd0e2712269957144b697b8d8a3ce586865f0aa0
reszkojr <s.reszkojr@gmail.com>
2023-10-17 05:47:46 -0300

    send back message to consumer & classroom_code route

--- classroom/consumers.py
+++ classroom/consumers.py
@@ -5,12 +5,14 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChannelConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print(f'{self.scope}')
         await self.accept()
 
     async def disconnect(self, close_code):
         print(f'{self.scope["user"]} disconnected')
         pass
 
-    async def receive(self, data):
-        print(data)
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        msg = text_data_json['message']
+
+        await self.send(text_data=msg)
