bd6d29e1009aa10fa649f1d546bfa9af30ea80c6
reszkojr <s.reszkojr@gmail.com>
2023-10-31 02:04:00 -0300

    changed access token lifetime to 5 days.

--- manthano_backend/settings.py
+++ manthano_backend/settings.py
@@ -58,7 +58,7 @@ CHANNEL_LAYERS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
 }
 
@@ -85,6 +85,11 @@ REST_FRAMEWORK = {
 ROOT_URLCONF = "manthano_backend.urls"
 
 CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:5173',
     'http://127.0.0.1:5173',
