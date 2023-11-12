4f062814d0fdb12a69a61b2094718318c488f370
reszkojr <s.reszkojr@gmail.com>
2023-11-12 15:29:56 -0300

    make access and refresh token longer

--- manthano_backend/settings.py
+++ manthano_backend/settings.py
@@ -58,8 +58,8 @@ CHANNEL_LAYERS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
 }
 
 MIDDLEWARE = [
