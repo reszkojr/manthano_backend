c233b6509f5bcd50b9c65329f96fb24aa0ee6491
reszkojr <s.reszkojr@gmail.com>
2023-11-12 15:46:39 -0300

    create a Profile on user creation

---
+++ authentication/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from authentication.models import ManthanoUser, Profile
+
+
+@receiver(post_save, sender=ManthanoUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
