ff9353206f0b12492322a44dc2ffc453159ba282
reszkojr <s.reszkojr@gmail.com>
2024-02-28 01:37:21 -0300

    JaaS JWT creation and retrieval

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,7 +8,9 @@ from authentication import views
 urlpatterns = [
     path("register/", views.RegisterView.as_view()),
     path("token/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/jaas", views.RetrieveJaasToken.as_view(), name="jaas_jwt"),
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/check", TokenVerifyView.as_view(), name="token_check"),
-    path("me/", views.UserInformation.as_view(), name="my_profile"), 
+    path("me/", views.UserInformation.as_view(), name="my_profile"),
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,14 @@
+import os
 from rest_framework import views, permissions, status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+from pathlib import Path
+
 from authentication.models import ManthanoUser
+from .jaasjwt import JaaSJwtBuilder
 from . import serializers
 
 
@@ -32,3 +36,32 @@ class UserInformation(APIView):
     def get(self, request):
         serializer = serializers.UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class RetrieveJaasToken(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        from decouple import config
+
+        try:
+            BASE_DIR = Path(__file__).resolve().parent.parent
+            fp = os.path.join(BASE_DIR, 'jitsi-key.pem')
+            private_key = open(fp, 'r')
+        except Exception as e:
+            print(e)
+            return Response('There was an error while reading Jitsi private API key.', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        user = request.user
+
+        jaasJWT = JaaSJwtBuilder()
+
+        token = jaasJWT.withDefaults() \
+            .withUserName(user.username) \
+            .withUserEmail(user.email) \
+            .withUserId(user.id) \
+            .withApiKey(config('JITSI_API_KEY')) \
+            .withAppID(config('JITSI_APP_ID')) \
+            .signWith(private_key.read())
+
+        return Response(token)
